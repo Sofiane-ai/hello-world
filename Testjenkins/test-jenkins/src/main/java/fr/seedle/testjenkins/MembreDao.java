d2e093069a16003864b6c4797979d1724bf47256
Sofiane-ai <65662407+Sofiane-ai@users.noreply.github.com>
2020-08-10 14:30:46 +0200

    Add files via upload

---
+++ Testjenkins/test-jenkins/src/main/java/fr/seedle/testjenkins/MembreDao.java
@@ -0,0 +1,9 @@
+package fr.seedle.testjenkins;
+
+public class MembreDao {
+	public static Membre getMembre() {
+		Membre membre = new Membre();
+		membre.setPrenom("Sofiane");
+		return membre; 
+	}
+}
